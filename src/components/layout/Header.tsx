4970ba885499e5c0006c645d335ceff7138f26be
bbeetlesam <185041685+bbeetlesam@users.noreply.github.com>
2025-11-19 04:07:00 +0700

    set the basic header structure
    left centre right section with proper navlinks

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,18 +1,28 @@
+import { NavLink } from 'react-router-dom';
 import { faMoon, faSun } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 function Header() {
   return (
-    <header className="w-screen bg-slate-100">
-      <section className="flex">
+    <header className="bg-slate-100">
+      <section className="flex items-center justify-between px-6 py-4 w-full">
+        {/* left header */}
         <div className="">
           WYDO!
         </div>
+
+        {/* centre: navlinks */}
         <div className="">
           <nav>
-            Home, Predict, About
+            <ul className="flex gap-4">
+              <li><NavLink to="/" end>Home</NavLink></li>
+              <li><NavLink to="/predict">Predict!</NavLink></li>
+              <li><NavLink to="/about">About</NavLink></li>
+            </ul>
           </nav>
         </div>
+
+        {/* right header */}
         <div className="">
           <FontAwesomeIcon icon={faMoon}></FontAwesomeIcon>
           <FontAwesomeIcon icon={faSun}></FontAwesomeIcon>
