112782f356aa3e21ecf0aa12a289ff1ddb69cfa8
bbeetlesam <185041685+bbeetlesam@users.noreply.github.com>
2025-11-19 05:59:38 +0700

    feat: routing for App.tsx! hell yeah dude

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Route, Routes } from 'react-router-dom';
+import Footer from './components/layout/Footer';
+import Header from './components/layout/Header'
+import Home from './components/layout/Home'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo bg-yellow-500" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="max-w-[100rem] mx-auto px-16">
+      <Header />
+      <div className="px-4">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/predict" element={<Home />} />
+          <Route path="/about" element={<Home />} />
+        </Routes>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          ur deathcount is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test your sanity
-        </p>
-      </div>
-      <p className="read-the-docs">
-        blablabla stop the bullshit
-      </p>
-    </>
+      <Footer />
+    </div>
   )
 }
 
-export default App
+export default App;
