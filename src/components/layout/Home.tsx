0c8066c4b2c7df502a373c22bb5ea73b6c20249b
bbeetlesam <185041685+bbeetlesam@users.noreply.github.com>
2025-11-20 04:07:58 +0700

    add: Home's simple hero and some placeholders for y-overflow
    also applies language context
    
    minimalist, hate it

--- src/components/layout/Home.tsx
+++ src/components/layout/Home.tsx
@@ -1,33 +1,33 @@
-import { useState } from 'react'
-import reactLogo from '../../assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useLang } from '../../context/LangContext'
+
+function Row({ i }: { i: number }) {
+  return <>placeholder#{i + 1} </>;
+}
 
 function Home() {
-  const [count, setCount] = useState(0)
+  const { t } = useLang()
 
   return (
     <main>
-      whatevershebringsweswing
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo bg-yellow-500" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          ur deathcount is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test your sanity
-        </p>
-      </div>
-      <p className="read-the-docs">
-        blablabla ts thick as a brick dude
-      </p>
+      {/* hero */}
+      <section className="h-screen flex items-center justify-center">
+        <div className="flex flex-col items-center gap-6">
+          <span className="text-4xl font-bold">{t.page.home.hero}</span>
+          <span className="text-lg text-white-100">{t.page.home.subhero}</span>
+          <a
+            href="/predict"
+            className="inline-block px-4 py-2.5 bg-cyan-600 text-white rounded-full hover:bg-cyan-700 transition"
+            role="button"
+          >
+            {t.page.home.heroBtn}
+          </a>
+        </div>
+      </section>
+
+      {/* placeholder for future progress */}
+      {Array.from({ length: 1000 }).map((_, i) => (
+        <Row key={i} i={i} />
+      ))}
     </main>
   )
 }
