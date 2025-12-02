a6230d3906222fdd4ca2205a53d411207826465e
bbeetlesam <185041685+bbeetlesam@users.noreply.github.com>
2025-12-02 17:40:45 +0700

    feat: add about page and top scroll to App.tsx, tweaks the padding
    now it looks much much better

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,23 @@
 import { Route, Routes } from 'react-router-dom';
 import Footer from './components/layout/Footer';
 import Header from './components/layout/Header'
-import Home from './components/layout/Home'
+import HomePage from './components/layout/Home'
 import PredictPage from './components/layout/PredictPage';
+import AboutPage from './components/layout/AboutPage';
 import { LangProvider } from './context/LangContext';
+import ScrollToTop from './components/ScrollToTop';
 
 function App() {
   return (
-    <div className="max-w-[110rem] mx-auto px-16 min-h-screen flex flex-col overflow-hidden">
+    <div className="max-w-[110rem] mx-auto px-0 md:px-6 lg:px-16 min-h-screen flex flex-col overflow-hidden">
       <LangProvider>
+        <ScrollToTop />
         <Header />
         <div className="px-4">
           <Routes>
-            <Route path="/" element={<Home />} />
+            <Route path="/" element={<HomePage />} />
             <Route path="/predict" element={<PredictPage />} />
-            <Route path="/about" element={<Home />} />
+            <Route path="/about" element={<AboutPage />} />
           </Routes>
         </div>
         <Footer />
